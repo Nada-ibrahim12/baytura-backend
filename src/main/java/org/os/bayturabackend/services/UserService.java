c2aa522c0feb6c7fee9745371beafe2834d32620
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-10-02 00:07:11 +0300

    edit pfp url

--- src/main/java/org/os/bayturabackend/services/UserService.java
+++ src/main/java/org/os/bayturabackend/services/UserService.java
@@ -272,7 +272,7 @@ public class UserService {
                 cloudinary.uploader().destroy(user.getProfilePictureId(), ObjectUtils.emptyMap());
             }
 
-            user.setProfilePictureUrl("https://github.com/user-attachments/assets/e4c05593-81cc-4aca-b8e6-571128095fbc");
+            user.setProfilePictureUrl("https://videos.openai.com/vg-assets/assets%2Ftask_01k4v15jwsfhgr10pac5rk4e1r%2F1757548901_img_1.webp?st=2025-10-01T19%3A55%3A16Z&se=2025-10-07T20%3A55%3A16Z&sks=b&skt=2025-10-01T19%3A55%3A16Z&ske=2025-10-07T20%3A55%3A16Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=b4ab33b8-2ad4-40af-8ed0-a2b350b6603c&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=IA7Ejbrk1yQSdAGEZfpc%2FOVkYygEBIVcddxK3vEQZeg%3D&az=oaivgprodscus");
             user.setProfilePictureId(null);
 
         }
