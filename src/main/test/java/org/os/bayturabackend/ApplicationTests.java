f190a0a3f4a59fb8c61e52e9295751775ccf949e
malak mohamed <malakmadbouly69@gmail.com>
2025-10-02 19:07:22 +0300

    lastEdit

---
+++ src/main/test/java/org/os/bayturabackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.os.bayturabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
